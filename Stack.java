550da77cb1096d1f0961a41f28f51516239ccc8d
XavMarq <kyle_xavier_marquez@dlsu.edu.ph>
2026-07-30 18:10:29 +0800

    Initialize Stack with Node class and constructor

--- Stack.java
+++ Stack.java
@@ -1,1 +1,19 @@
+public class Stack<T> {
+  private class Node {
+    T data;
+    Node next;
 
+    public Node(T data) {
+      this.data = data;
+      this.next = null;
+    }
+  }
+  
+  private Node top;
+  private int count;
+
+  public Stack() {
+    this.top = null;
+    this.count = 0;
+  }
+}  
