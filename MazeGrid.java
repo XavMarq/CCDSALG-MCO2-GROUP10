3422cbaf07e11f28959e1e02f7fc6c617adf9c3b
eivannchua <eivannchua@gmail.com>
2026-08-01 02:05:40 +0800

    Create MazeGrid.java

---
+++ MazeGrid.java
@@ -0,0 +1,55 @@
+public class MazeGrid 
+{
+    private int rows;
+    private int cols;
+    private Cell[][] grid;
+    private Cell start;
+    private Cell goal;
+
+    public MazeGrid(int rows, int cols) 
+    {
+        this.rows = rows;
+        this.cols = cols;
+        this.grid = new Cell[rows][cols];
+    }
+
+    public Cell getCell(int x, int y) 
+    {
+        return this.grid[x][y];
+    }
+
+    public int getRows() 
+    {
+        return this.rows;
+    }
+
+    public int getCols() 
+    {
+        return this.cols;
+    }
+
+    public Cell getStart() 
+    {
+        return this.start;
+    }
+    
+    public Cell getGoal() 
+    {
+        return this.goal;
+    }
+    
+    public void setCell(int x, int y, Cell cell) 
+    {
+        this.grid[x][y] = cell;
+        
+        if (cell.getType() == 'S') 
+        {
+            this.start = cell;
+        } 
+
+        else if (cell.getType() == 'G') 
+        {
+            this.goal = cell;
+        }
+    }
+}
