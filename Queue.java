d7d1a15a6dc2d1775c02020607af15c24c0a0a63
XavMarq <kyle_xavier_marquez@dlsu.edu.ph>
2026-07-31 18:49:10 +0800

    Initialize Queue with Node class and constructor

--- Queue.java
+++ Queue.java
@@ -1,1 +1,21 @@
+public class Queue<T> {
+  private class Node {
+    T data;
+    Node next;
 
+    public Node(T data) {
+      this.data = data;
+      this.next = null;
+    }
+  }
+
+  private Node front;
+  private Node rear;
+  private int count;
+
+  public Queue() {
+    this.front = null;
+    this.rear = null;
+    this.count = 0;
+  }
+}
