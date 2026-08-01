7d8296abad22cb82086dcb3d8b42009623d99f45
XavMarq <kyle_xavier_marquez@dlsu.edu.ph>
2026-08-01 19:20:58 +0800

    Add enqueue, dequeue, peek, and isEmpty methods

--- Queue.java
+++ Queue.java
@@ -18,4 +18,52 @@ public class Queue<T> {
     this.rear = null;
     this.count = 0;
   }
+
+  public boolean isEmpty() {
+    boolean empty = false;
+    if (this.front == null) {
+      empty = true;
+    }
+    reutrn empty;
+  }
+
+  public void enqueue(T data) {
+    Node newNode = new Node(data);
+
+    if (this.rear == null) {
+      this.front = newNode;
+      this.rear = newNode;
+    } else {
+      this.rear.next = newNode;
+      this.rear. = newNode;
+    }
+    this.count++;
+  }
+
+  public void dequeue() {
+    T dequeuedData = null;
+
+    if (this.front != null) {
+      dequeuedData - this.front.data;
+      this.front = this.front.next;
+
+      if (this.front == null) {
+        this.rear = null;
+      }
+      this.count--;
+    }
+    
+    public T peek() {
+      T frontData = null;
+      
+      if (this.front != null) {
+        frontData = this.front.data;
+      }
+      return frontData;
+    }
+    
+    public int getCount() {
+      return this.count;
+    }
 }
+
