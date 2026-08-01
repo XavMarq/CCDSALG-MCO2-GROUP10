38429e2db24cf04523955b2c06b69503b3ba1bf8
ZeroPR1 <52110485+ZeroPR1@users.noreply.github.com>
2026-08-01 19:26:35 +0800

    Add pathFound variable and constructor to Pathfinder

--- Pathfinder.java
+++ Pathfinder.java
@@ -3,6 +3,14 @@ public class Pathfinder
     private int totalSteps;
     private int pathLength;
     private long executionTime;
+    private boolean pathFound;
+
+    public Pathfinder() {
+        this.totalSteps = 0;
+        this.pathLength = 0;
+        this.executionTime = 0;
+        this.pathFound = false;
+    }
 
 
 
