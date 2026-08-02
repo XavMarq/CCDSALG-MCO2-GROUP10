4f978beffdcae447648e10bfa5439ced433a2e48
eivannchua <eivannchua@gmail.com>
2026-08-02 18:32:27 +0800

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -1,92 +1,71 @@
-/**
- * Cell class that represents a cell in the maze.
- * Stores its x and y position, type, and visited cells.
- */
-public class Cell 
+public class Cell
 {
-    /**
-     * Row position of the cell
-     */
     private int x;
-
-    /**
-     * Column position of the cell
-     */
     private int y;
-
-    /**
-     * Cell type (' ', '#', 'S', or 'G')
-     */
     private char type;
-
-    /**
-     * che the cell has been visited.
-     */
     private boolean visited;
+    private boolean backtracked;
+    private boolean solutionPath;
 
-    /**
-     * Constructor that creates a Cell object given its x and y position and type.
-     * Initializes the cell's x and y coordinates, type, and sets the initial visited to false;
-     *
-     * @param x the starting x-coordinate of the cell
-     * @param y the starting y-coordinate of the cell
-     * @param type the character representing the cell type
-     */
-    public Cell(int x, int y, char type) 
+    public Cell(int x, int y, char type)
     {
         this.x = x;
         this.y = y;
         this.type = type;
         this.visited = false;
+        this.backtracked = false;
+        this.solutionPath = false;
     }
 
-    /**
-     * Getter that retrieves the x-coordinate of the cell
-     *
-     * @return the x-coordinate of the cell
-     */
-    public int getX() 
+    public int getX()
     {
         return this.x;
     }
 
-    /**
-     * Getter that retrieves the y-coordinate of the cell
-     *
-     * @return the y-coordinate of the cell
-     */
-    public int getY() 
+    public int getY()
     {
         return this.y;
     }
 
-    /**
-     * Getter that retrieves the cell type
-     *
-     * @return the cell type
-     */
-    public char getType() 
+    public char getType()
     {
         return this.type;
     }
 
-    /**
-     * Getter that retrieves the visited state of the cell
-     *
-     * @return true if the cell has been visited; false otherwise
-     */
-    public boolean isVisited() 
+    public boolean isVisited()
     {
         return this.visited;
     }
 
-    /**
-     * Setter that updates the visited state of the cell
-     *
-     * @param visited the cell's new visited state 
-     */
-    public void setVisited(boolean visited) 
+    public boolean isBacktracked()
+    {
+        return this.backtracked;
+    }
+
+    public boolean isSolutionPath()
+    {
+        return this.solutionPath;
+    }
+
+    public void setVisited(boolean visited)
     {
         this.visited = visited;
+    }
+
+    public void setBacktracked(boolean backtracked)
+    {
+        this.backtracked = backtracked;
+    }
+
+    public void setSolutionPath(boolean solutionPath)
+    {
+        this.solutionPath = solutionPath;
+    }
+
+    public void resetSearchState()
+    {
+        this.visited = false;
+        this.backtracked = false;
+        this.solutionPath = false;
     }
 }
