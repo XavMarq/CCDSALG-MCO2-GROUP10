bc0b2fc40c0dd78c20723397f120f834ae7db5d1
eivannchua <eivannchua@gmail.com>
2026-08-02 18:39:09 +0800

    Update Pathfinder.java

--- Pathfinder.java
+++ Pathfinder.java
@@ -1,108 +1,170 @@
-public class Pathfinder 
+public class PathFinder
 {
     private int totalSteps;
     private int pathLength;
-    private long executionTime;
+    private double executionTime;
     private boolean pathFound;
 
-    public Pathfinder() {
+    public PathFinder()
+    {
         this.totalSteps = 0;
         this.pathLength = 0;
-        this.executionTime = 0;
+        this.executionTime = 0.0;
         this.pathFound = false;
     }
 
-    public boolean solveDFS(MazeGrid maze) {
-        boolean found = false;
-        long startTime = System.currentTimeMillis();
+    public boolean solveDFS(MazeGrid maze, UserInterface ui, int delayMilliseconds)
+    {
+        this.totalSteps = 0;
+        this.pathLength = 0;
+        this.executionTime = 0.0;
+        this.pathFound = false;
+        maze.resetSearchState();
 
+        boolean found = false;
+        long algorithmNanoseconds = 0L;
         Stack<Cell> stack = new Stack<Cell>();
         Cell start = maze.getStart();
+        Cell goal = maze.getGoal();
 
-        if (start != null) {
+        if (start != null && goal != null)
+        {
+            long stepStartTime = System.nanoTime();
             stack.push(start);
             start.setVisited(true);
+            this.totalSteps = 1;
+            long stepEndTime = System.nanoTime();
+            algorithmNanoseconds += stepEndTime - stepStartTime;
+
+            ui.animateStep(maze, start, delayMilliseconds);
         }
 
-        while (!stack.isEmpty() && !found) {
+        while (!stack.isEmpty() && !found)
+        {
+            long stepStartTime = System.nanoTime();
             Cell current = stack.peek();
-            this.totalSteps++;
+            Cell displayedCell = current;
 
-            if (current.getType() == 'G') {
+            if (current.getType() == 'G')
+            {
                 found = true;
             }
-            else {
+            else
+            {
                 Cell neighbor = getUnvisitedNeighbor(maze, current);
 
-                if (neighbor != null) {
+                if (neighbor != null)
+                {
                     neighbor.setVisited(true);
                     stack.push(neighbor);
+                    this.totalSteps++;
+                    displayedCell = neighbor;
                 }
-                else {
+                else
+                {
+                    current.setBacktracked(true);
                     stack.pop();
+                    displayedCell = stack.peek();
                 }
+            }
+
+            long stepEndTime = System.nanoTime();
+            algorithmNanoseconds += stepEndTime - stepStartTime;
+
+            if (!found)
+            {
+                ui.animateStep(maze, displayedCell, delayMilliseconds);
             }
         }
 
-        long endTime = System.currentTimeMillis();
-        this.executionTime = endTime - startTime;
-        this.pathLength = stack.getCount();
-        
+        if (found)
+        {
+            this.pathLength = stack.getCount() - 1;
+
+            while (!stack.isEmpty())
+            {
+                Cell pathCell = stack.pop();
+                pathCell.setSolutionPath(true);
+            }
+        }
+
+        this.pathFound = found;
+        this.executionTime = algorithmNanoseconds / 1000000.0;
+
         return found;
     }
 
-
-    private Cell getUnvisitedNeighbor(MazeGrid maze, Cell current) {
+    private Cell getUnvisitedNeighbor(MazeGrid maze, Cell current)
+    {
         int x = current.getX();
         int y = current.getY();
         Cell neighbor = null;
         boolean found = false;
 
-        if (!found && x - 1 >= 0) {
+        if (!found && x - 1 >= 0)
+        {
             Cell up = maze.getCell(x - 1, y);
-            if (up != null && up.getType() != '#' && !up.isVisited()) {
+
+            if (up.getType() != '#' && !up.isVisited())
+            {
                 neighbor = up;
                 found = true;
             }
         }
 
-        if (!found && x + 1 < maze.getRows()) {
+        if (!found && x + 1 < maze.getRows())
+        {
             Cell down = maze.getCell(x + 1, y);
-            if (down != null && down.getType() != '#' && !down.isVisited()) {
+
+            if (down.getType() != '#' && !down.isVisited())
+            {
                 neighbor = down;
                 found = true;
             }
         }
-        
-        if (!found && y - 1 >= 0) {
+
+        if (!found && y - 1 >= 0)
+        {
             Cell left = maze.getCell(x, y - 1);
-            if (left != null && left.getType() != '#' && !left.isVisited()) {
+
+            if (left.getType() != '#' && !left.isVisited())
+            {
                 neighbor = left;
                 found = true;
             }
         }
 
-        if (!found && y + 1 < maze.getCols()) {
+        if (!found && y + 1 < maze.getCols())
+        {
             Cell right = maze.getCell(x, y + 1);
-            if (right != null && right.getType() != '#' && !right.isVisited()) {
+
+            if (right.getType() != '#' && !right.isVisited())
+            {
                 neighbor = right;
                 found = true;
             }
         }
+
         return neighbor;
     }
-    
-    public int getTotalSteps() {
+
+    public int getTotalSteps()
+    {
         return this.totalSteps;
     }
 
-    public long getExecutionTime() {
+    public int getPathLength()
+    {
+        return this.pathLength;
+    }
+
+    public double getExecutionTime()
+    {
         return this.executionTime;
     }
 
-    
-
-
-    // search algo 
-   
+    public boolean isPathFound()
+    {
+        return this.pathFound;
+    }
 }
